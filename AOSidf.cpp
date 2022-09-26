42ca267b416a3a166fcf6db58ddabe8661501608
sukrisl <laksono.sukri@gmail.com>
2022-09-27 01:06:48 +0700

    fix(AOSidf): fix send event failed from entry action

--- AOSidf.cpp
+++ AOSidf.cpp
@@ -6,8 +6,8 @@ void AOSidf::_init() {
 }
 
 void AOSidf::start(const char* aoname, int32_t queueLen, uint8_t priority, uint32_t stackSize, std::shared_ptr<HSMidf_state> initState) {
-    this->transitionTo(initState);
     AOidf::start(aoname, queueLen, priority, stackSize);
+    this->transitionTo(initState);
 }
 
 void AOSidf::transitionTo(std::shared_ptr<HSMidf_state> state) {
