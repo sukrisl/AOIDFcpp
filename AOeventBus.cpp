9bed470c932491e0c45da19c70626c91de2037fd
sukrisl <laksono.sukri@gmail.com>
2022-09-29 14:02:05 +0700

    refactor(AOSeventBus): remove dedicated event subscriber

--- AOeventBus.cpp
+++ AOeventBus.cpp
@@ -4,20 +4,7 @@
 
 #include "AOeventBus.h"
 
-#define EVENT_ASSERT(event, flag) { do { if (!event) { ESP_LOGE(TAG, "Event 0x%04x has not created, call AOeventBus::createEvent() first.", flag); abort(); } } while(0); }
-
 static const char* TAG = "ao_evt_bus";
-
-void AOevent::notify() {
-    ESP_LOGW(TAG, "Event received (0x%04x)", this->_flag);
-
-    for (std::list<std::shared_ptr<AOidf>>::iterator i = _subscribers.begin(); i != _subscribers.end(); ++i) {
-        char subsName[20];
-        (*i)->getName(subsName);
-        ESP_LOGI(TAG, "Posting (0x%04x) to %s", this->_flag, subsName);
-        (*i)->post(this->_flag, NULL, 0);
-    }
-}
 
 void AOeventBus::_init() {
     // Default: do nothing
@@ -28,31 +15,18 @@ void AOeventBus::_deinit() {
 }
 
 void AOeventBus::dispatch(uint32_t eventFlag, void* eventData) {
-    EVENT_ASSERT(_eventList[eventFlag], eventFlag);
-    _eventList[eventFlag]->notify();
+    for (uint32_t i = 0; i < _subscribers.size(); i++) {
+        char subsName[20];
+        _subscribers[i]->getName(subsName);
+        ESP_LOGI(TAG, "Posting (0x%04x) to %s", eventFlag, subsName);
+        _subscribers[i]->post(eventFlag);
+    }
 }
 
-void AOeventBus::createEvent(uint32_t flag) {
-    std::shared_ptr<AOevent> eventBuf = std::make_shared<AOevent>(flag);
-    this->subscribe(flag);
-    _eventList.insert(std::make_pair(flag, eventBuf));
+void AOeventBus::attach(std::shared_ptr<AOidf> subscriber) {
+    _subscribers.push_back(subscriber);
 }
 
-void AOeventBus::createEvent(uint32_t flag, std::shared_ptr<AOidf> subscriber) {
-    this->createEvent(flag);
-    this->attach(flag, subscriber);
-}
-
-void AOeventBus::attach(uint32_t flag, std::shared_ptr<AOidf> subscriber) {
-    EVENT_ASSERT(_eventList[flag], flag);
-
-    _eventList[flag]->attach(subscriber);
-    subscriber->subscribe(flag);
-}
-
-void AOeventBus::detach(uint32_t flag, std::shared_ptr<AOidf> subscriber) {
-    EVENT_ASSERT(_eventList[flag], flag);
-
-    _eventList[flag]->detach(subscriber);
-    subscriber->unsubscribe(flag);
+void AOeventBus::detach(std::shared_ptr<AOidf> subscriber) {
+    _subscribers.erase(std::remove(_subscribers.begin(), _subscribers.end(), subscriber), _subscribers.end());
 }
--- include/AOeventBus.h
+++ include/AOeventBus.h
@@ -1,42 +1,20 @@
 #pragma once
 
-#include <list>
+#include <algorithm>
 #include <memory>
-#include <unordered_map>
+#include <vector>
 
 #include "AOidf.h"
 
-class AOevent {
- private:
-    std::list<std::shared_ptr<AOidf>> _subscribers;
-
- public:
-    uint32_t _flag;
-
-    AOevent(uint32_t flag) : _flag(flag) {}
-
-    void attach(std::shared_ptr<AOidf> subscriber) {
-        _subscribers.push_back(subscriber);
-    }
-
-    void detach(std::shared_ptr<AOidf> subscriber) {
-        _subscribers.remove(subscriber);
-    }
-
-    void notify();
-};
-
 class AOeventBus : public AOidf {
  private:
-    std::unordered_map<uint32_t, std::shared_ptr<AOevent>> _eventList;
+    std::vector<std::shared_ptr<AOidf>> _subscribers;
 
     void _init() override;
     void _deinit() override;
     void dispatch(uint32_t eventFlag, void* eventData) override;
 
  public:
-    void createEvent(uint32_t flag);
-    void createEvent(uint32_t flag, std::shared_ptr<AOidf> subscriber);
-    void attach(uint32_t flag, std::shared_ptr<AOidf> subscriber);
-    void detach(uint32_t flag, std::shared_ptr<AOidf> subscriber);
+    void attach(std::shared_ptr<AOidf> subscriber);
+    void detach(std::shared_ptr<AOidf> subscriber);
 };
